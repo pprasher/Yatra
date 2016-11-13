bd8d5a80c97584fa882c17152e6b75ecc321d3e0
shuchirinamdar <inamdar.shuchir77@gmail.com>
2016-11-12 23:10:15 -0700

    adding changes to py crawler

--- YatraCrawler/VehicleDataCrawler.py
+++ YatraCrawler/VehicleDataCrawler.py
@@ -15,10 +15,11 @@ def main():
                 "http://www.fueleconomy.gov/feg/PowerSearch.do?action=noform&year=2010&make=Toyota&pageno=1&sortBy=Comb&tabView=0&rowLimit=200");
     urls.insert(4,
                 "http://www.fueleconomy.gov/feg/PowerSearch.do?action=noform&year=2010&make=Suzuki&pageno=1&sortBy=Comb&tabView=0&rowLimit=200");
-    #urls.insert(5,"http://www.fueleconomy.gov/feg/PowerSearch.do?action=noform&year=2015&make=Toyota&pageno=1&sortBy=Comb&tabView=0&rowLimit=200");
-    urls.insert(5,
+
+    urls.insert(5,"http://www.fueleconomy.gov/feg/PowerSearch.do?action=noform&year=2015&make=Toyota&pageno=1&sortBy=Comb&tabView=0&rowLimit=200");
+    urls.insert(6,
                 "http://www.fueleconomy.gov/feg/PowerSearch.do?action=noform&year=2015&make=Suzuki&pageno=1&sortBy=Comb&tabView=0&rowLimit=200");
-    #urls.insert(3,"http://www.fueleconomy.gov/feg/PowerSearch.do?action=noform&year=2005&make=Honda&pageno=1&sortBy=Comb&tabView=0&rowLimit=200");
+    urls.insert(7,"http://www.fueleconomy.gov/feg/PowerSearch.do?action=noform&year=2005&make=Honda&pageno=1&sortBy=Comb&tabView=0&rowLimit=200");
 
     tablehead = 'CarModel,EngineDisplacement,Transmission,FuelType,ImgUrl,MPG,UserEco\n'
     while len(urls) > 0:
@@ -46,17 +47,22 @@ def main():
 
                     if row.find('td', attrs={'class': 'mpg-epa'}):
                         imgurl = row.find('td', attrs={'class': 'vphoto'}).find('img', attrs={'class': 'img-thumbnail img-responsive veh-photo'})
-                        fuelecoTable = row.find('td', attrs={'class': 'mpg-epa'}).find('div',attrs={'class': 'panel panel-default mpg-border'}).find('div',attrs={'class':'panel-body'}).find('table',attrs={'class':'results'}).findAll('tbody')
-                        fueleco = ''
-                        for fuelecorow in fuelecoTable:
-                            fueleco = fuelecorow.find('td', attrs={'class': 'mpg-comb'})
-                            break
-
-                        usereco = row.find('td', attrs={'class': 'mpg-user'})
-                        tablehead = tablehead + ',' + str(imgurl['src'])
-                        tablehead = tablehead + ',' + str(fueleco.text)
-                        tablehead = tablehead + ',' + str(usereco.text)
-                        tablehead = tablehead + '\n'
+                        fuelecoTable = ''
+                        try:
+                            fuelecoTable = row.find('td', attrs={'class': 'mpg-epa'}).find('div',attrs={'class': 'panel panel-default mpg-border'}).find('div',attrs={'class':'panel-body'}).find('table',attrs={'class':'results'}).findAll('tbody')
+                            fueleco = ''
+                            for fuelecorow in fuelecoTable:
+                                fueleco = fuelecorow.find('td', attrs={'class': 'mpg-comb'})
+                                break
+                        except:
+                            pass
+                        finally:
+                            if fuelecoTable is not '':
+                                usereco = row.find('td', attrs={'class': 'mpg-user'})
+                                tablehead = tablehead + ',' + str(imgurl['src'])
+                                tablehead = tablehead + ',' + str(fueleco.text)
+                                tablehead = tablehead + ',' + str(usereco.text)
+                                tablehead = tablehead + '\n'
                     rowNum += 1
         #tablehead = tablehead + '\n'
     tablehead = tablehead + '\n'
